ab8d6d6078a4e776aa4ee112f0d2797e658b8dac
hwanvely <990706leo@gmail.com>
2025-11-18 16:25:09 +0900

    feat: 게시글 관련 모델 구현

---
+++ src/comments/model/comment.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, ForeignKey, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    member_id = Column(Integer, ForeignKey('member.id'), nullable=False)
+    content = Column(Text, nullable=False)
+
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    post = relationship("Post", backref="comments")
+    member = relationship("Member", backref="comments")
--- src/member/model/member.py
+++ src/member/model/member.py
@@ -1,14 +1,19 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class Member(Base):
-    __tablename__ = "members"
+    __tablename__ = "member"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     email = Column(String(100), unique=True, nullable=False, index=True)
     password = Column(String(255), nullable=False)
     nickname = Column(String(50), nullable=False)
     profile_image = Column(String(255), nullable=True)
+
+    posts = relationship("Post", backref="author")
+    liked_posts = relationship("PostLike", back_populates="member")
+    comments = relationship("Comment", back_populates="author")
